e278175e18d7b06180e522a722a692eb9dd69474
Stenio Oliveira <steniooliv@gmail.com>
2021-05-28 15:19:17 -0300

    change domain to session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,6 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'ability-accounting-api.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app'
+  # domain: 'https://ability-accounting-api.herokuapp.com/api/'
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
