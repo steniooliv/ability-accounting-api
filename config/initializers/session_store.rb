9a7295441475b9a53be53224b675e40151e6a7a4
Stenio Oliveira <steniooliv@gmail.com>
2021-05-28 18:00:43 -0300

    add session secure

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,11 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app', expire_after: 14.days
+  Rails.application.config.session_store
+    :cookie_store,
+    key: '_authentication_app',
+    secure: Rails.env.production?,
+    ttponly: true,
+    domain: :all,
+    expire_after: 14.days
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
