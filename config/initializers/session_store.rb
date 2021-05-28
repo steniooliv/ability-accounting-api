a0ad0653ddd81633b148e40324ccdf04b7278842
Stenio Oliveira <steniooliv@gmail.com>
2021-05-28 15:34:46 -0300

    session store domain fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app'
-  # domain: 'https://ability-accounting-api.herokuapp.com/api/'
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: '172.29.91.153'
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
+
