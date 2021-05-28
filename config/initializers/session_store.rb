6e03c2a3f71cc16db6563ed14d7583286a4de779
Stenio Oliveira <steniooliv@gmail.com>
2021-05-28 16:37:22 -0300

    change session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,13 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, same_site: :none, key: '_authentication_app', domain: :all
+  
+  Rails.application.config.session_store :cookie_store, {
+    :key => '_authentication_app',
+    :domain => :all,
+    :same_site => :none,
+    :secure => :true,
+    :tld_length => 2
+  }
+
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
