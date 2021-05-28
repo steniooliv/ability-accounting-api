079a49f6702c5c1972d21c218812db118fd37ba0
Stenio Oliveira <steniooliv@gmail.com>
2021-05-28 16:32:25 -0300

    fix session storage cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, :same_site :none, key: '_authentication_app', domain: :all
+  Rails.application.config.session_store :cookie_store, same_site: :none, key: '_authentication_app', domain: :all
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
