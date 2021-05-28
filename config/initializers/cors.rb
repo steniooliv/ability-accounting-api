ba20d87d3f050b310aace4d5010140b8eba73a86
Stenio Oliveira <steniooliv@gmail.com>
2021-05-28 15:49:11 -0300

    change cors domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://172.29.91.153:3000"
+    origins "http://hm.ls:3000"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
