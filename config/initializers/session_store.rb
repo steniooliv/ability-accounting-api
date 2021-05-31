1895e5f70741703756f13de7ee61bfe32be7be07
Stenio Oliveira <steniooliv@gmail.com>
2021-05-31 16:16:07 -0300

    fix session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,3 @@
 Rails.application.config.session_store :cookie_store, key: '_ability_app', domain: :all, tld_length: 2
+Rails.application.config.action_dispatch.cookies_serializer = :json
+Rails.application.config.secret_token = 'abaccapp'
