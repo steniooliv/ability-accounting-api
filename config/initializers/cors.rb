d0ddf2e6fa7d188d0d7348837be8ec7680d7afbe
Stenio Oliveira <steniooliv@gmail.com>
2021-06-09 10:27:58 -0300

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://172.21.190.242:3000"
+    origins "http://172.21.188.53:3000"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
