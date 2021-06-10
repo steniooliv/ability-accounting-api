e586b251817f2b5949404d1543cad56a618b2656
Stenio Oliveira <steniooliv@gmail.com>
2021-06-10 14:42:30 -0300

    remove coments lines

--- app/controllers/users_registrations_controller.rb
+++ app/controllers/users_registrations_controller.rb
@@ -19,5 +19,31 @@ class UsersRegistrationsController < ApplicationController
       render json: {status: 500}
     end
   end
+
+  # def show
+  #   user = User.find(params[:id])
+
+  #   if user.id
+  #     render json: {user: user}
+  #   else
+  #     render json: {status: 404}
+  #   end
+
+  # end
+
+  # def index
+  #   @users = User.all
+  #   render json: {users: @users}
+  # end
+
+  # def update
+  #   user = User.find(params[:id])
+  #   user.update!(params.require(:user).permit(:name, :email, :password, :password_confirmation, :accounting_id))
+
+  # end
+
+  # def destroy
+  #   user = User.delete(params[:id])
+  # end
   
 end
