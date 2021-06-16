343cb71998565d8340ded10efe2c8c9106a8b74d
Stenio Oliveira <steniooliv@gmail.com>
2021-06-16 17:01:14 -0300

    add invoice products

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -20,8 +20,20 @@ class CompaniesController < ApplicationController
       @current_company = @companies.find(params['id'])
       
       @invoices = Invoice.all.where(company_id: @current_company.id)
-      
+
       render json: {company: @current_company, invoices: @invoices.as_json(include: :customer)}
+
+    else
+      render json: {status: 404}
+    end
+  end
+
+  def invoice_products
+    if session[:user_id]
+
+      @invoice_products = InvoiceProduct.all
+
+      render json: {invoice_products: @invoice_products}
 
     else
       render json: {status: 404}
