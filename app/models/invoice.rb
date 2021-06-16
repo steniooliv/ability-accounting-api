b9b363529ff60819d47d75eeb515fab57ec826a6
Stenio Oliveira <steniooliv@gmail.com>
2021-06-16 17:02:13 -0300

    add relationship with invoice products

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,10 +2,13 @@ class Invoice < ApplicationRecord
   belongs_to :company
   belongs_to :customer
 
-  validates :number, :serie, :total_value, presence: true
+  validates :customer_id, :company_id, :number, :serie, :model, :type_record, :type_movement, :date_issue, :date_departure, :access_key, :total_product, :discount_value, :expenses_value, :shipping_value, :safe_value, :icms_base, :icms_value, :sticms_base, :sticms_value, :ipi_base, :ipi_value, :pis_base, :pis_value, :cofins_base, :cofins_value, :invoice_value, presence: true
+
+  alias_attribute :name, :number
 
   has_many :companies
   has_many :customers
+  has_many :invoice_products
 
   has_one_attached :invoice_xml
 end
