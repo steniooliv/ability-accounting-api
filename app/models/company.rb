a135315d4b19a39b4b66c3e252b9f95eb1a3ec27
Stenio Oliveira <steniooliv@gmail.com>
2021-06-16 17:01:28 -0300

    create number field

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,7 @@
 class Company < ApplicationRecord
   belongs_to :accounting
 
-  validates :name, presence: true
+  validates :name, :number, presence: true
 
   has_many :accountings
   has_many :invoices
